dc90c61893d0d04d03b2ef825478bbfcb8c17106
laith <lith_2o17a@hotmail.com>
2024-07-28 13:27:07 +0300

    JSX

---
+++ FirstReactWithJSX/firstjsx/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hello Dojo</h1>
+      <h2>Things i need to do</h2>
+      <li>Learn React</li>
+      <li>Climb Mt.Event</li>
+      <li>Run a Marathon</li>
+      <li>Feed the dogs</li>
+    </>
+  
+  );
+}
+
+export default App;
